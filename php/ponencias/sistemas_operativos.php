ab888e150656b97349c4adb9d0280c640c9d8d2d
Jogua <josegua93@gmail.com>
2015-04-23 15:15:40 +0200

    ponencia añadida

--- php/ponencias/sistemas_operativos.php
+++ php/ponencias/sistemas_operativos.php
@@ -2,15 +2,32 @@
 
 if ($id == 13) {
     echo " <p> <strong>Sistemas Windows,</strong> <i>por A. León</i> </p>";
-    echo " <p>  </p>";
-    echo " <p>  </p>";
+    echo " <p>Los Sistemas Windows son aquellos que pertenecen a la familia de sistemas operativos desarrollados y vendidos
+        por Microsoft. Se distribuyen versiones para PC, smartphone, servidores y sistemas empotrados.
+        Microsoft introdujo un entorno operativo denominado Windows el 20 de noviembre de 1985 como un complemento para MS-DOS
+        en respuesta al creciente interés en las interfaces gráficas de usuario (GUI).</p>";
+    echo " <p>Microsoft Windows llegó a dominar el mercado mundial de computadoras personales, con más del 90% de la cuota de
+        mercado, superando a Mac OS, que había sido introducido en 1984.</p>";
+    echo " <p>Windows 10 es la versión más reciente del sistema operativo desarrollado por Microsoft. Será el sucesor de
+        Windows 8 y 8.1. El sistema operativo se anuncia sólo dos años después del lanzamiento de Windows 8, en octubre de
+        2012, una versión que supuso un cambio radical respecto a las anteriores de Windows y que ha tenido durante este
+        tiempo una recepción mixta por parte de los usuarios y las empresas.</p>";
 } else if ($id == 14) {
     echo " <p> <strong>Sistemas Unix/Linux,</strong> <i>por P. Paderewski</i> </p>";
-    echo " <p>  </p>";
-    echo " <p>  </p>";
+    echo " <p>Unix es un Sistema operativo portable, multitarea y multiusuario; desarrollado, en principio, en 1969, por un
+        grupo de empleados de los laboratorios Bell de AT&T.</p>";
+    echo " <p>s empleados para referirse a la combinación del núcleo o kernel libre similar a Unix denominado Linux con el
+        sistema GNU. Su desarrollo es uno de los ejemplos más prominentes de software libre; todo su código fuente puede ser
+        utilizado, modificado y redistribuido libremente por cualquiera bajo los términos de la GPL y otra serie de licencias
+        libres.</p>";
 } else if ($id == 15) {
     echo " <p> <strong>Sistemas iOS/Mac,</strong> <i>por R. Montes</i> </p>";
-    echo " <p>  </p>";
-    echo " <p>  </p>";
+    echo " <p>iOS es un sistema operativo móvil de la multinacional Apple Inc. Originalmente desarrollado para el iPhone
+        (iPhone OS), después se ha usado en dispositivos como el iPod touch y el iPad. No permite la instalación de iOS
+        en hardware de terceros. Los elementos de control consisten de deslizadores, interruptores y botones. La respuesta
+        a las órdenes del usuario es inmediata y provee una interfaz fluida.</p>";
+    echo " <p>OS X, antes llamado Mac OS X, es un entorno operativo basado en Unix, desarrollado, comercializado y vendido
+        por Apple Inc. Ha sido incluido en su gama de computadoras Macintosh desde el año de 2002. OS X es el sucesor del 
+        Mac OS 9 (la versión final del Mac OS Classic), el sistema operativo de Apple desde 1984.</p>";
 }
 ?>
